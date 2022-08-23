61ba314d44d792c90d642a5f3280400c8c7d78d2
Jakob Hauser <jakob.e.hauser@protonmail.com>
2022-08-23 11:40:39 +0200

    wrote script for topic modelling

--- src/Bertopic/topic_model_articles.py
+++ src/Bertopic/topic_model_articles.py
@@ -13,16 +13,9 @@ db_path = (data_path / "data_bases" / "all_occupations.db")
 con = sqlite3.connect(db_path)
 cur = con.cursor()
 
-docs = {}
-
-rev_dirs = ["15-1211", "15-1221", "15-1231", "15-1242",
-            "15-1244", "15-1251", "15-1252", "15-1253", "15-1254"]
-
-rev_dirs = ["15-1211", "15-1221", "15-1231", "15-1242",
-            "15-1244", "15-1251", "15-1252", "15-1253", "15-1254"]
-
 cur.execute(
     "SELECT occ_code, occ_title, strict_links, rev_dirs FROM occupations WHERE occ_group = 'detailed'")
+
 query_result = cur.fetchall()
 occupations = {}
 
@@ -86,40 +79,8 @@ for occ_str in query_result:
                 [all_paragraphs.append(p) for p in paragraphs]
 
 
-                
-
-
-
-
-
-# for rev_dir in rev_dirs:
-#     rev_dir_path = revisions_path / rev_dir
-#     page_dirs = os.listdir(rev_dir_path)
-
-#     for page_name in page_dirs:
-#         docs[page_name] = {}
-#         page_dir_path = rev_dir_path / page_name
-#         rev_file_list = os.listdir(page_dir_path)
-
-#         for file_name in rev_file_list[::5]:
-#             file_path = page_dir_path / file_name
-#             with open(file_path, "r") as rev_file:
-#                 rev = json.load(rev_file)
-#                 docs["page_name"].append(rev)
-
-
 topic_model = BERTopic(language="english",calculate_probabilities=True, verbose=True)
-topic_model.fit(all_pages)
+topic_model.fit(all_paragraphs)
 
 
 
-# topics_1 = topic_model.transform(doc_1)
-# topics_2 = topic_model.transform(doc_2)
-# topic_probs_1 = np.argsort(topics_1[1])[::-1]
-# topic_probs_2 = np.argsort(topics_2[1])[::-1]
-# topic_set_1 = set(list(topic_probs_1[0][:20]))
-# topic_set_2 = set(list(topic_probs_2[0][:20]))
-
-# topic_diff = topic_set_2 - topic_set_1
-# freq = topic_model.get_topic_info()
-# print(freq[freq.Topic.isin(list(topic_diff))])
